9e65927f816eca2762a5f110c45592aeb039d289
Larisa Yushkevich <yushkevichlar@gmail.com>
2022-03-09 00:40:22 +0300

    added menu rendering & projects search bar

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", () => {
-	// adding projects section items
+	//====================== rendering projects section items ======================
 	class projectsBlock {
-		constructor(name, about, icon) {
+		constructor(name, description, icon) {
 			this.name = name;
-			this.about = about;
+			this.description = description;
 			this.icon = icon;
 		}
 	}
 
-	let addBlock = (name, about, icon) => blocks.push(new projectsBlock(name, about, icon));
+	let addBlock = (name, description, icon) => projectsBlocks.push(new projectsBlock(name, description, icon));
 
-	let blocks = new Array();
+	let projectsBlocks = new Array();
 
 	addBlock("Spring Boot", "Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible.", "./icons/spring-boot.svg");
 	addBlock("Spring Framework", "Provides core support for dependency injection, transaction management, web apps, data access, messaging, and more.", "./icons/spring-framework.svg");
@@ -23,40 +23,312 @@ window.addEventListener("DOMContentLoaded", () => {
 
 	let projectsContent = document.querySelector(".projects__wrapper");
 
-	(function (blocksArray) {
+	/**
+	 *
+	 * @param {array} blocksArray
+	 * @returns
+	 */
+	function renderProjectsSection(blocksArray) {
+		while (projectsContent.firstChild) projectsContent.firstChild.remove();
+
 		if (blocksArray.length === 0) {
 			projectsContent.innerHTML = "<div class='projects__no-results'>No results</div>";
 			return;
 		}
 
 		blocksArray.map(block => {
-			let projectsItem = document.createElement("a");
-			projectsItem.href = "#";
-			projectsItem.className = "projects__item";
+			// let projectsItem = document.createElement("a");
+			// projectsItem.href = "#";
+			// projectsItem.className = "projects__item";
 
-			let projectIcon = document.createElement("div");
-			projectIcon.className = "project__icon";
+			// let projectIcon = document.createElement("div");
+			// projectIcon.className = "project__icon";
 
-			let img = document.createElement("img");
-			img.src = block.icon;
-			img.alt = "project";
-			img.className = "project__icon-img";
+			// let img = document.createElement("img");
+			// img.src = block.icon;
+			// img.alt = "project";
+			// img.className = "project__icon-img";
 
-			let projectText = document.createElement("div");
-			projectText.className = "project__text";
+			// let projectText = document.createElement("div");
+			// projectText.className = "project__text";
 
-			let h3 = document.createElement("h4");
-			h3.innerHTML = block.name;
+			// let h3 = document.createElement("h4");
+			// h3.innerHTML = block.name;
 
-			let p = document.createElement("p");
-			p.innerHTML = block.about;
+			// let p = document.createElement("p");
+			// p.innerHTML = block.description;
 
-			projectsItem.append(projectIcon);
-			projectIcon.append(img);
-			projectsItem.append(projectText);
-			projectText.append(h3);
-			projectText.append(p);
-			projectsContent.append(projectsItem);
+			// projectsItem.append(projectIcon);
+			// projectIcon.append(img);
+			// projectsItem.append(projectText);
+			// projectText.append(h3);
+			// projectText.append(p);
+			// projectsContent.append(projectsItem);
+			projectsContent.innerHTML += `
+				<a class="projects__item" href="#">
+					<div class="project__icon">
+						<img class="project__icon-img" src="${block.icon}" alt="project">
+					</div>
+					<div class="project__text">
+						<h3>${block.name}</h3>
+						<p>${block.description}</p>
+					</div>
+				</a>
+			`;
 		});
-	})(blocks);
+	}
+
+	renderProjectsSection(projectsBlocks);
+
+	//====================== mobile menu listeners ======================
+
+	const mobileNav = document.querySelector(".nav__mobile-items"),
+		hamburger = document.querySelector(".nav__mobile-hamburger"),
+		mobileNavClose = document.querySelector(".nav__mobile-close");
+
+	hamburger.addEventListener("click", () => {
+		mobileNav.classList.toggle("js-display-block");
+		hamburger.classList.add("js-display-none");
+		mobileNavClose.classList.toggle("js-display-block");
+	});
+
+	mobileNavClose.addEventListener("click", () => {
+		mobileNav.classList.toggle("js-display-block");
+		hamburger.classList.remove("js-display-none");
+		hamburger.classList.add("js-display-block");
+		mobileNavClose.classList.toggle("js-display-block");
+	});
+
+	mobileNav.onclick = e => {
+		let target = e.target;
+		let parent = target.closest(".mobile-category");
+		parent.querySelector(".mobile-nav-arrow").classList.toggle("flipped");
+
+		switch (target.id) {
+			case "mobile-why":
+				let mobWhy = document.getElementById("mobile-why-items");
+				mobWhy.classList.toggle("js-display-none");
+				break;
+			case "mobile-learn":
+				let mobLearn = document.getElementById("mobile-learn-items");
+				mobLearn.classList.toggle("js-display-none");
+				break;
+			case "mobile-projects":
+				let mobProject = document.getElementById("mobile-project-items");
+				mobProject.classList.toggle("js-display-none");
+				break;
+			case "mobile-community":
+				let mobCommunity = document.getElementById("mobile-community-items");
+				mobCommunity.classList.toggle("js-display-none");
+				break;
+			default:
+				break;
+		}
+	};
+
+	//====================== rendering menus ======================
+
+	let nav = [
+		{
+			title: "Why Spring",
+			items: ["Overview", "Microservices", "Reactive", "Event Driven", "Cloud", "Web Applications", "Serverless", "Batch"],
+		},
+		{
+			title: "Learn",
+			items: ["Overview", "Quickstart", "Guides", "Blog"],
+		},
+		{
+			title: "Projects",
+			items: [
+				"Overview",
+				"Spring Boot",
+				"Spring",
+				"Spring Framework",
+				"Spring Cloud",
+				"Spring Cloud Data Flow",
+				"Spring Data",
+				"Spring Integration",
+				"Spring Batch",
+				"Spring Security",
+				"View all projects",
+				"Development Tools",
+				"Spring Tools 4",
+				"Spring Initializr",
+			],
+		},
+		{
+			title: "Training",
+			items: [],
+		},
+		{
+			title: "Support",
+			items: [],
+		},
+		{
+			title: "Community",
+			items: ["Overview", "Events", "Team"],
+		},
+	];
+
+	let mobWhy = document.getElementById("mobile-why"),
+		mobLearn = document.getElementById("mobile-learn"),
+		mobProject = document.getElementById("mobile-projects"),
+		mobTraining = document.getElementById("mobile-training"),
+		mobSupport = document.getElementById("mobile-support"),
+		mobCommunity = document.getElementById("mobile-community"),
+		mobTitles = [];
+
+	mobTitles.push(mobWhy, mobLearn, mobProject, mobTraining, mobSupport, mobCommunity);
+
+	let mobWhyItems = document.getElementById("mobile-why-items"),
+		mobLearnItems = document.getElementById("mobile-learn-items"),
+		mobProjectItems = document.getElementById("mobile-project-items"),
+		mobCommunityItems = document.getElementById("mobile-community-items"),
+		mobItems = [];
+
+	mobItems.push(mobWhyItems, mobLearnItems, mobProjectItems, null, null, mobCommunityItems);
+
+	let menuTitles = document.querySelectorAll(".nav__items .nav__dropdown");
+	let menuItems = document.querySelectorAll(".nav__dropdown-content-list");
+
+	/**
+	 *
+	 * @param {object} elements
+	 */
+	function renderMenu(elements) {
+		for (let i = 0; i < elements.length; i++) {
+			if (elements[i].title === "Training" || elements[i].title === "Support") {
+				mobTitles[i].innerHTML += `
+				    <div class="mobile-title">${elements[i].title}</div>
+			    `;
+			} else {
+				mobTitles[i].innerHTML += `
+                    <div class="mobile-title">${elements[i].title}</div>
+                    <div class="mobile-nav-arrow"></div>
+			    `;
+			}
+		}
+
+		for (let i = 0; i < elements.length; i++) {
+			if (elements[i].title === "Training" || elements[i].title === "Support") {
+				menuTitles[i].insertAdjacentHTML(
+					"afterbegin",
+					`
+					<a class="nav__items-link" href="#">${elements[i].title}</a>
+			    `
+				);
+			} else {
+				menuTitles[i].insertAdjacentHTML(
+					"afterbegin",
+					`
+				    <span>${elements[i].title}</span>
+			    `
+				);
+			}
+		}
+	}
+
+	/**
+	 *
+	 * @param {object} elements
+	 */
+	function renderDropdownMenu(elements) {
+		for (let i = 0; i < elements.length; i++) {
+			let itemsArr = elements[i].items;
+
+			for (let j = 0; j < itemsArr.length; j++) {
+				menuItems[i].innerHTML += `
+					<li><a href='#'>${itemsArr[j]}</a></li>
+				`;
+				mobItems[i].innerHTML += `
+					<li><a href='#'>${itemsArr[j]}</a></li>
+				`;
+				mobItems[i].classList.add("js-display-none");
+			}
+		}
+	}
+
+	renderMenu(nav);
+	renderDropdownMenu(nav);
+
+	document.querySelector("#mobile-project-items li:nth-child(12)").classList.add("mobile-nav-uppercase");
+	document.querySelector("#mobile-project-items li:nth-child(14) a").innerHTML += `<svg class='external-link-icon'
+		xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+		<polyline points="15 10.94 15 15 1 15 1 1 5.06 1" fill="none" stroke="currentColor"
+			stroke-miterlimit="10" stroke-width="2" />
+		<polyline points="8.93 1 15 1 15 7.07" fill="none" stroke="currentColor"
+			stroke-miterlimit="10" stroke-width="2" />
+		<line x1="15" y1="1" x2="8" y2="8" fill="none" stroke="currentColor"
+			stroke-miterlimit="10" stroke-width="2" />
+	</svg>`;
+
+	document.querySelector("#nav__dropdown-content-list-projects li:nth-child(11) a").classList.add("nav__dropdown-content_blue");
+	document.querySelector("#nav__dropdown-content-list-projects li:nth-child(12)").classList.add("nav__dropdown-content-subtitle");
+	document.querySelector("#nav__dropdown-content-list-projects li:nth-child(14) a").innerHTML += `
+	<svg class="nav__dropdown-link-icon" xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 16 16">
+		<polyline points="15 10.94 15 15 1 15 1 1 5.06 1" fill="none"
+			stroke="currentColor" stroke-miterlimit="10" stroke-width="2" />
+		<polyline points="8.93 1 15 1 15 7.07" fill="none" stroke="currentColor"
+			stroke-miterlimit="10" stroke-width="2" />
+		<line x1="15" y1="1" x2="8" y2="8" fill="none" stroke="currentColor"
+			stroke-miterlimit="10" stroke-width="2" />
+	</svg>`;
+
+	//====================== search bar projects ======================
+	/**
+	 *
+	 * @param {string} text
+	 */
+	function searchProjects(text) {
+		let inputValue = text;
+
+		if (inputValue.trim() !== "") {
+			let newProjects = projectsBlocks.filter(elem => elem.name.toLowerCase().includes(inputValue.toLowerCase()) || elem.description.toString().includes(inputValue));
+			console.log(newProjects);
+			renderProjectsSection(newProjects);
+		} else {
+			renderProjectsSection(projectsBlocks);
+		}
+	}
+
+	/**
+	 *
+	 * @param {object} e
+	 */
+	function handleSearchInputChange(e) {
+		let inputValue = e.currentTarget.value;
+		if (e.key === "Enter") {
+			searchProjects(inputValue);
+		} else if (inputValue === "") {
+			document.querySelector("#resetButton").classList.add("js-visibility-hidden");
+		} else if (inputValue !== "") {
+			document.querySelector("#resetButton").classList.remove("js-visibility-hidden");
+		}
+	}
+
+	let timer;
+	document.querySelector("#input").addEventListener("keyup", e => {
+		let inputValue = e.currentTarget.value;
+		clearTimeout(timer);
+
+		timer = setTimeout(() => {
+			searchProjects(inputValue);
+		}, 3000);
+	});
+
+	document.querySelector("#searchIcon").addEventListener("click", e => {
+		let inputValue = document.querySelector("#input").value;
+		console.log(inputValue);
+		searchProjects(inputValue);
+	});
+
+	document.querySelector("#resetButton").addEventListener("click", () => {
+		document.querySelector("#input").value = "";
+		document.querySelector("#resetButton").classList.add("js-visibility-hidden");
+		renderProjectsSection(projectsBlocks);
+	});
+
+	document.querySelector("#input").addEventListener("keydown", event => handleSearchInputChange(event));
+	document.querySelector("#input").addEventListener("change", event => handleSearchInputChange(event));
 });
